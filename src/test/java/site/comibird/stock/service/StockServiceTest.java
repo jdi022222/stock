3015c9c857bcc425f954f0911ea3405a8b3ce98e
jdi022222 <jdi022222@gmail.com>
2024-02-29 01:21:05 +0900

    add 동시에 100개 재고감소 test

--- src/test/java/site/comibird/stock/service/StockServiceTest.java
+++ src/test/java/site/comibird/stock/service/StockServiceTest.java
@@ -2,6 +2,10 @@ package site.comibird.stock.service;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -34,6 +38,29 @@ class StockServiceTest {
 	void 재고감소() throws Exception {
 		stockService.decrease(1L, 1L);
 		Stock stock = stockRepository.findById(1L).orElseThrow();
-		assertEquals(stock.getQuantity(), 99L);
+		assertEquals(99L, stock.getQuantity());
+	}
+
+	@Test
+	void 동시에_100개의_재고감소() throws Exception {
+		int threadCount = 100;
+		ExecutorService executorService = Executors.newFixedThreadPool(32);// 비동기 작업을 단순화
+		CountDownLatch latch = new CountDownLatch(threadCount); // 다른 스레드에서 작업이 완료될 때까지 기다릴 수 있도록 해주는 클래스
+
+		for (int i = 0; i < threadCount; i++) {
+			executorService.submit(() -> {
+				try {
+					stockService.decrease(1L, 1L);
+				} finally {
+					latch.countDown();
+				}
+			});
+		}
+
+		latch.await();
+		executorService.shutdown();
+
+		Stock stock = stockRepository.findById(1L).orElseThrow();
+		assertEquals(0L, stock.getQuantity());
 	}
 }
